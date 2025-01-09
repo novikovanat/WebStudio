dd577884bd9e9cdf82601dea52a8fe72714c19b9
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-01-09 21:15:25 +0100

    navigation added

--- menu.js
+++ menu.js
@@ -1,24 +1,24 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector('[data-modal-open-menu]'),
-        closeModalBtn: document.querySelector('[data-modal-close-menu]'),
-        modal: document.querySelector('[data-modal-menu]'),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-menu]'),
+    closeModalBtn: document.querySelector('[data-modal-close-menu]'),
+    modal: document.querySelector('[data-modal-menu]'),
+    mobilePortfolioLink: document.querySelector('[data-mobile-portfolio-link]'),
+  };
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.mobilePortfolioLink.addEventListener('click', closeModal);
 
-    window.addEventListener("orientationchange", function(event) {
-        closeModal();
-    });
+  window.addEventListener('orientationchange', function (event) {
+    closeModal();
+  });
 
-    function toggleModal() {
-        refs.modal.classList.toggle('is-open');
-    }
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
 
-    function closeModal() {
-        refs.modal.classList.add('is-hidden');
-    }
-
-
+  function closeModal() {
+    refs.modal.classList.remove('is-open');
+  }
 })();
